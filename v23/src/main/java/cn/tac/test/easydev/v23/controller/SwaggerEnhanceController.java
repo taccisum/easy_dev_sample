0811a4b0263be23fdca1bcbf4aaa64a811f650ff
taccisum <514162920@qq.com>
2018-06-04 19:22:34 +0800

    feature sample: swagger enhance

---
+++ v23/src/main/java/cn/tac/test/easydev/v23/controller/SwaggerEnhanceController.java
@@ -0,0 +1,27 @@
+package cn.tac.test.easydev.v23.controller;
+
+import cn.tac.framework.easydev.web.response.wrapper.annotation.GenericResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author tac
+ * @since 2018/6/4
+ */
+@RestController
+@RequestMapping("swagger_enhance")
+public class SwaggerEnhanceController {
+    @GetMapping("echo_token")
+    @GenericResponse("echo token")
+    public String echoToken(HttpServletRequest request) {
+        String token = request.getHeader("token");
+        if (StringUtils.isBlank(token)) {
+            return "empty token";
+        }
+        return token;
+    }
+}
